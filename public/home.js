08a09c82c63c56978248afdf6855ed71c37e8717
tomas <perezgantomas@gmail.com>
2018-06-05 23:15:17 -0400

    home.js not displaying info

--- public/home.js
+++ public/home.js
@@ -1,6 +1,15 @@
 "use strict";
 {
-  let search = {
-    templateUrl: "home.html",
+  let home = {
+  let getAllItems = () => {
+    return $http({
+      url: "/api/items",
+      method: "GET"
+    }).then((response) => {
+      return response.itmes;
+    })
   }
+}
+  angular
+  .service("home", home);
 }
